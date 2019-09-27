0bd35355680e3d39c4e268ac2a38cc8e73cf1572
xyzopowa <xyzopowa@gmail.com>
2019-09-27 13:53:16 +0200

    init

---
+++ check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+_MASTER="master1.priv.xyzo.fr master2.priv.xyzo.fr master3.priv.xyzo.fr"
+_WORKER="worker1.priv.xyzo.fr worker2.priv.xyzo.fr worker3.priv.xyzo.fr"
+for i in ${_MASTER} ${_WORKER}
+do
+ssh -o strickthostkeychecking=no $i hostname
+done
